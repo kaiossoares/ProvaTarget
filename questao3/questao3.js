c892b479a967b7d4ed3043317d5af5618fb777ed
Kaio Antonio <kaioantoniosoares@gmail.com>
2023-03-20 22:02:57 -0300

    questao3

--- questao3/questao3.js
+++ questao3/questao3.js
@@ -1,3 +1,18 @@
-let valor = 2
+const dados = require('./dados.json')
+const vetorJson = dados.map(objeto => objeto.valor)
 
-if (valor = 2) print("valor é 2")
+const vetor = vetorJson.filter(valor => valor !== 0)
+
+const menorValor = Math.min(...vetor)
+const maiorValor = Math.max(...vetor)
+
+// Calcula a média dos valores do array "valores"
+const soma = vetor.reduce((total, valor) => total + valor, 0);
+const media = soma / vetor.length;
+
+let diasAcimaDaMedia = 0
+for(let i = 0; i<vetor.length; i++) {
+    if(vetor[i] > media) diasAcimaDaMedia ++
+}
+
+console.log(`Maior Valor: ${maiorValor}\nMenor Valor: ${menorValor}\nNúmero de dias com faturamento maior que a média: ${diasAcimaDaMedia}`);
